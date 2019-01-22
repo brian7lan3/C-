89ee995ea9595fd2cfedbc36d5361eadf631db4a
BrianLan <brian7lan3@gmail.com>
2019-01-22 18:02:09 +0800

    函數2

--- Function/function1.cpp
+++ Function/function1.cpp
@@ -8,10 +8,17 @@ long adder(int x, int y)
 	return x + y;
 }
 
+void greeting(void)
+{
+	cout << "HI HI HI ! ! ! " << endl;
+}
+
 
 int main()
 {
 	int value1 = 5, value2 = 10;
+	
+	greeting();
 	
 	cout << adder(value1, value2) << endl;
 	cout << adder(3, 4) << endl;
