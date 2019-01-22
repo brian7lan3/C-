a2d97c677f84ad89bb37a04e591177094efb50b9
BrianLan <brian7lan3@gmail.com>
2019-01-22 17:02:49 +0800

    sizeof將以位元組來表示陣列占用的總位元數

--- Array/array8.cpp
+++ Array/array8.cpp
@@ -4,19 +4,13 @@ using namespace std;
 
 int main()
 {
-	int test_index, student_index, scores[3][5] = {
-		{ 92 , 73 , 57 , 98 , 89},
-		{ 88 , 76 , 23 , 95 , 72},
-		{ 94 , 82 , 63 , 99 , 94},
-	};
+	int scores[5] = {92 , 73 , 57 , 98 , 89 };
 	
-	float sum;
+	cout << sizeof(scores) << endl;		//sizeof將以位元組來表示陣列占用的總位元數 
 	
-	for (test_index = 0; test_index < 3; test_index++) {
-		for (student_index = 0, sum = 0; student_index < 5; student_index++)
-			sum = sum + scores[test_index][student_index];
-		cout << "Average for test " << test_index + 1 << " is " << sum / 5 << scores[1][2] << endl;
-	}
+	cout << sizeof(int) << endl;		
+	
+	cout << sizeof(scores) / sizeof(int) << endl;		//The array has 5 elements 
 
 	return 0;
 }
