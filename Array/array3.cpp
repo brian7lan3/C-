b3b08a5ea4a59a4c698a4dbc495d00c605018228
BrianLan <brian7lan3@gmail.com>
2019-01-22 16:11:25 +0800

    C風格字串

---
+++ Array/array3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char word[6];
+	word[0] = 'H';
+	word[1] = 'e';
+	word[2] = 'l';
+	word[3] = 'l';
+	word[4] = 'o';
+	word[5] = '\0';
+	
+	
+	cout << word << endl;		//後面不用加[] 
+	return 0;
+}
