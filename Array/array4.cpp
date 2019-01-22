5cbde9c2c9d64beedefd097050aec3caf28347b3
BrianLan <brian7lan3@gmail.com>
2019-01-22 16:14:45 +0800

    改變陣列寫法

---
+++ Array/array4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char word[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+	char other_word[] = "HELLO";
+	
+	cout << word << endl;
+	cout << other_word << endl;
+	return 0;
+}
