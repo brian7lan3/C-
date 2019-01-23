cccaef4bcaaabf3c0c8d2184da5d538c64f8d8c9
BrianLan <brian7lan3@gmail.com>
2019-01-23 11:32:18 +0800

    答案好像快要出來了 ，再加油

--- Homework/homework0123b.cpp
+++ Homework/homework0123b.cpp
@@ -11,40 +11,24 @@ using namespace std;
 
 int main()
 {
-	int i = 0;
-	int Array[MAX];
-	for (i = 0; i < MAX; i++) Array[i] = i;
-	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("Array[%d] = %d\n", i , Array[i]);
-	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("*(Array+%d) = %d\n", i , *(Array+i));
-	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("&Array[%d] = %0X\n", i , &Array[i]);
-	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("(Array+%d) = %0X\n", i , (Array+i));
+//	int i = 0;
+//	int Array[MAX];
+//	for (i = 0; i < MAX; i++) Array[i] = i;
+//	
+//	int *ptr = &Array[0];
 	
 	
+	struct structure {
+		int name_id;
+	};
 	
-	printf("\n\n");
-	int *ptr1 = &Array[0];
+	structure people[3];
 	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("ptr1[%d] = %d\n", i , ptr1[i]);
+	structure *pointer = &people[3];
 	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("*(ptr1+%d) = %d\n", i , *(ptr1+i));
+	pointer -> name_id = 2222;
 	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("&ptr1[%d] = %0X\n", i , &ptr1[i]);
-	
-	printf("======================================== \n");
-	for (i = 0; i< MAX; i++) printf("(ptr1+%d) = %0X\n", i , (ptr1+i));
-
-	
+	cout << pointer -> name_id << endl;
 
 	return 0;
 }
