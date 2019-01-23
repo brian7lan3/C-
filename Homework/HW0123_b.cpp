9ff0e77d9803aed88163de278d56d4c63e001b70
BrianLan <brian7lan3@gmail.com>
2019-01-23 15:22:59 +0800

    //可以從第一個元素，出發 指向任意一個元素

--- Homework/HW0123_b.cpp
+++ Homework/HW0123_b.cpp
@@ -18,7 +18,9 @@ int main()
 	double *pointer = &values[0];		//現在，獲得了一個指向第一個元素的指標!!! 
 	
 	cout << "指標 指向第一個元素 : " << *pointer << endl; 
-	cout << "指標 指向第二個元素 : " << *(++pointer) << endl; 
+	cout << "指標 指向第二個元素 : " << *(pointer + 1) << endl; 
+	cout << "指標 指向第三個元素 : " << *(pointer + 2) << endl; 
+	cout << "指標 指向第四個元素 : " << *(pointer + 3) << endl; //可以從第一個元素，出發 指向任意一個元素 
 	 
 	 
 	
