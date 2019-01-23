9470036de70311655986c77a2bf2ed49b82317af
BrianLan <brian7lan3@gmail.com>
2019-01-23 15:21:17 +0800

    //現在，獲得了一個指向第一個元素的指標!!!

--- Homework/HW0123_b.cpp
+++ Homework/HW0123_b.cpp
@@ -13,8 +13,12 @@ int main()
 	// 2. 用指標的方式，把結構指入進去
 	
 	
-	double values[4] = {0.0 , 1.0 , 2.0 , 3.0};
+	double values[4] = {0.55 , 1.1 , 2.22 , 3.333};
+	
 	double *pointer = &values[0];		//現在，獲得了一個指向第一個元素的指標!!! 
+	
+	cout << "指標 指向第一個元素 : " << *pointer << endl; 
+	cout << "指標 指向第二個元素 : " << *(++pointer) << endl; 
 	 
 	 
 	
