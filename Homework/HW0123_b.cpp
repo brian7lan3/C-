cac17e401af1483486891ca1a45e658df59a17dd
BrianLan <brian7lan3@gmail.com>
2019-01-23 15:18:06 +0800

    //現在，獲得了一個指向第一個元素的指標!!!

---
+++ Homework/HW0123_b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	// 1. 先創建一個陣列
+	
+	// 2. 用指標的方式，把結構指入進去
+	
+	
+	double values[4] = {0.0 , 1.0 , 2.0 , 3.0};
+	double *pointer = &values[0];		//現在，獲得了一個指向第一個元素的指標!!! 
+	 
+	 
+	
+
+	return 0;
+}
