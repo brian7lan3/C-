cc9eb6d373714a9ece0cd57754e0f03861a42b49
BrianLan <brian7lan3@gmail.com>
2019-01-23 14:23:36 +0800

    答案吧

--- Homework/homework0123b.cpp
+++ Homework/homework0123b.cpp
@@ -17,15 +17,14 @@ int main()
 	structure *pointer = &people[3];
 	
 	
-	pointer -> name_id = 111;
-	(pointer+1) -> name_id = 222;
-	(pointer+2) -> name_id = 333;
+	pointer -> name_id = 1;
+	(pointer+1) -> name_id = 2;
+	(pointer+2) -> name_id = 3;
 	
 	cout << pointer -> name_id << endl;
-	cout << (pointer+1)-> name_id << endl;
+	cout << (pointer+1) -> name_id << endl;
 	cout << (pointer+2) -> name_id << endl;
-
-
+	
 
 	return 0;
 }
