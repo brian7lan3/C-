1acad85d692c3a574392d8c146de24daa3c9f8ba
BrianLan <brian7lan3@gmail.com>
2019-01-24 09:12:06 +0800

    怪怪的

---
+++ Function/function2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+int main()		//¦b¨ç¼Æ¦Wmain 
+{
+	
+	void greeting();
+	
+	
+	return 0;
+}
+
+void greeting(void)
+{
+	cout << "Hello fromthe greeting function function." << endl;
+}
