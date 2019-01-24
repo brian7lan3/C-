c585545fca4c2e1ca0226aabe1d3e331a45ccc26
BrianLan <brian7lan3@gmail.com>
2019-01-24 09:21:38 +0800

    //函數順序有差別

--- Function/function2.cpp
+++ Function/function2.cpp
@@ -6,20 +6,26 @@ using namespace std;
 
 
 
-void greeting(void)
+void greeting(void)		//函數的順序有差別，這個函數要在上面 (不然會錯誤 
 {
 	cout << "000HI HI HI ! ! ! " << endl;
 }
 
 
+long adder(int x, int y)
+{
+	return x + y;
+}
+
+
 int main()
 {
-	//int value1 = 5, value2 = 10;
+	int value1 = 5, value2 = 10;
 	
 	greeting();
 	
-	//cout << adder(value1, value2) << endl;
-	//cout << adder(3, 4) << endl;
+	cout << adder(value1, value2) << endl;
+	cout << adder(3, 4) << endl;
 	
 	return 0;
 }
