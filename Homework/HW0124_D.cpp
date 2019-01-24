f6146dc7147c7bf9d9cd0311a4e28a87361c3c15
BrianLan <brian7lan3@gmail.com>
2019-01-24 16:27:32 +0800

    應該可以吧

--- Homework/HW0124_D.cpp
+++ Homework/HW0124_D.cpp
@@ -12,7 +12,9 @@ int main()
 	struct structure{
 		int student_id;
 		struct structure *next;
-	}structure1, structure2, structure3;
+	};
+	structure structure1, structure2, structure3;
+	structure *pointer = &structure1;
 	
 	structure1.next = &structure2;
 	structure2.next = &structure3;
@@ -21,14 +23,14 @@ int main()
 	int array[3];
 	
 	
-	structure1.student_id = 1111;
-	array[0] = structure1.student_id;
+	pointer -> student_id = 1111;
+	array[0] = pointer -> student_id;
 	
-	structure1.next -> student_id = 2222;
-	array[1] = structure1.next -> student_id;
+	pointer -> next -> student_id = 2222;
+	array[1] = pointer -> next -> student_id;
 	
-	structure1.next -> next -> student_id = 3333;
-	array[2] = structure1.next -> next -> student_id;
+	pointer -> next -> next -> student_id = 3333;
+	array[2] = pointer -> next -> next -> student_id;
 	
 	cout << array[0] << endl;
 	cout << array[1] << endl;
@@ -37,3 +39,5 @@ int main()
 
 	return 0;
 }
+
+
