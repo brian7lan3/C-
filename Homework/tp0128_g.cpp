5bf5079f613d35674272c8f50a823c1d0efdfa24
BrianLan <brian7lan3@gmail.com>
2019-01-28 13:53:05 +0800

    整理

---
+++ Homework/tp0128_g.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main(){
+	
+	struct S{
+		int id;
+	}s[3];
+	
+	S *pointer1 = &s[0];
+	S *pointer2 = &s[1];
+	S *pointer3 = &s[2];
+	
+	pointer1 -> id = 93;
+	pointer2 -> id = 91;
+	pointer3 -> id = 97;
+		
+	int array[1000];
+	int *ptr = &array[0];
+	
+	*ptr = pointer1 -> id;
+	*(ptr + 1) = pointer2 -> id;
+	*(ptr + 2) = pointer3 -> id;
+	
+	cout << *ptr << endl;
+	cout << *(ptr + 1) << endl;
+	cout << *(ptr + 2) << endl;
+	
+	cout << endl;
+	
+	sort(array, array + 3);
+	cout << *ptr << endl;
+	cout << *(ptr + 1) << endl;
+	cout << *(ptr + 2) << endl;
+	
+	reverse(array, array + 3);
+	cout << *ptr << endl;
+	cout << *(ptr + 1) << endl;
+	cout << *(ptr + 2) << endl;
+	
+	return 0;
+}
+
+
