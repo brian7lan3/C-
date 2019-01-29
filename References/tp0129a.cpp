a4064a3bd41dd9cae8110527446b44c4c7ccc090
BrianLan <brian7lan3@gmail.com>
2019-01-29 11:27:13 +0800

    tp/a 在函數的參數列中的參數前加上&時，這個參數就是以傳址方式傳遞的。這表示可以直接存取這個參數，不必透過間接存取指標

---
+++ References/tp0129a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+
+void add_one_ptrs(long * n);
+
+void add_one_refs(long & n);
+
+int main(){
+	
+	long salary = 250000;
+	
+	cout << "Current salary: $" << salary << endl;
+	
+	add_one_ptrs(&salary);
+	
+	cout << "After the first raise: $" << salary << endl;
+	
+	add_one_refs(salary);
+	
+	cout << "After the second raise: $" << salary << endl;
+	
+	return 0;
+}
+
+
+void add_one_ptrs(long * n)
+{
+	(*n)++;
+}
+
+void add_one_refs(long & n)
+{
+	n++;
+}
+
