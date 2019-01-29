3ea07c6cdea40e874556887d2bd16a470ce8422e
BrianLan <brian7lan3@gmail.com>
2019-01-29 14:51:19 +0800

    tp/A 創建函數的框架就好久好久 呵呵

---
+++ Homework/tp0129_A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+struct S{
+	int id;
+}s[100];
+
+
+void add_structure(string new_s, int c1);
+
+int main(){
+	
+	
+	add_structure("n", 1);
+
+
+	return 0;
+}
+
+void add_structure(string new_s, int c1)
+{
+//	S *new_s = new S;
+//	tmp = s[c1];
+	
+}
