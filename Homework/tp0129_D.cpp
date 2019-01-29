e724ea9742c6481d3251ab4a26a9f18a4b3cb0ea
BrianLan <brian7lan3@gmail.com>
2019-01-29 17:37:09 +0800

    tp/D 結構裡面可以同時用兩種宣告方式

---
+++ Homework/tp0129_D.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+
+struct S
+{
+	S(void){};
+	
+	S(char cData,S *pNext)
+    {
+       data = cData;
+       next = pNext;
+    }
+    char data;
+    S *next;
+}w[100];
+
+
+//S *ptr = new S('c', 0 );
+
+
+int main(){
+	
+	S *ptr = new S('c', 0 );
+	
+	S *ptr2 = new S('d', ptr);
+	
+	S *ptr3 = new S('e', ptr2);
+	
+	S *ptr4 = new S('f', ptr3);
+	
+	cout << ptr -> data << endl;
+	cout << ptr -> next << endl;
+	
+
+
+	cout << ptr2 -> next -> data << endl;
+	cout << ptr2 -> next -> next << endl;
+	
+	
+	cout << ptr3 -> next -> next -> data << endl;
+	cout << ptr3 -> next -> next -> next << endl;
+	
+	cout << ptr4 -> next -> next -> next -> data << endl;
+	cout << ptr4 -> next -> next -> next -> next << endl;
+	return 0;
+}
