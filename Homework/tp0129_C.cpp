fe0e3561528fcd76020f96b4623c17c17b19a1ad
BrianLan <brian7lan3@gmail.com>
2019-01-29 17:12:55 +0800

    tp/C 成功把三個結構指標連結在一起了

---
+++ Homework/tp0129_C.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+
+struct S
+{
+	S(char cData,S *pNext)
+    {
+       data = cData;
+       next = pNext;
+    }
+    char data;
+    S *next;
+};
+
+
+//S *ptr = new S('c', 0 );
+
+
+int main(){
+	
+	S *ptr = new S('c', 0 );
+	
+	S *ptr2 = new S('d', ptr);
+	
+	S *ptr3 = new S('e', ptr2);
+	
+	cout << ptr -> data << endl;
+	cout << ptr -> next << endl;
+	
+
+
+	cout << ptr2 -> next -> data << endl;
+	cout << ptr2 -> next -> next << endl;
+	
+	
+	cout << ptr3 -> next -> next -> data << endl;
+	cout << ptr3 -> next -> next -> next << endl;
+	return 0;
+}
