10f1d0fa9859f79eb34cf9f0429a030c69480f9c
BrianLan <brian7lan3@gmail.com>
2019-01-31 14:58:00 +0800

    練習

---
+++ Tutorial/tp2_3Dim_ans.cpp
@@ -0,0 +1,33 @@
+#include <iostream>  
+using namespace std;
+
+class TwoDim{
+	int x, y;
+	
+	public:
+		void set2(int a, int b){x = a; y = b;}
+		void show(){cout << x << " " << y;}
+		
+};
+
+class ThreeDim: public TwoDim{
+	int z;
+	
+	public:
+		void set(int a, int b, int c){
+			TwoDim::set2(a, b);
+			z = c;
+		}
+		void show(){
+			TwoDim::show();
+			cout << " " << z << endl;
+		}
+};
+
+int main(){
+	ThreeDim point3D;
+	point3D.set2(1, 2);
+	point3D.show();
+	system("pause");
+	return 0;
+}
