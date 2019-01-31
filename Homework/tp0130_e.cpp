06a641ba2ebcb868dc1b688027050e3c34bdee37
BrianLan <brian7lan3@gmail.com>
2019-01-31 08:43:15 +0800

    tp2019/1/31 今天準備學習 那個C++教學的網頁版

--- Homework/tp0130_e.cpp
+++ Homework/tp0130_e.cpp
@@ -11,15 +11,30 @@ struct S
 	S *next;
 }w[10];
 
-void new_Structure(int value){
+void create_Structure(){
+	for(int i = 0; i < 10; i++){
+		w[i].next = &w[i + 1];
+	}
+}
+
+S* new_Structure(int value){
+	create_Structure();
+	
 	S *node = new S;
 	w[value].next = node;
 	node -> next = &w[value + 1];
+	return node;
 }
 
 int main(){
 	
 	new_Structure(1);
+	
+	node -> data = 'a';
+	
+	
+	
+
 	
 	return 0;
 }
---
+++ Homework/新文件7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle{
+	
+	public:
+		void setLength(int l);
+		void setWidth(int w);
+		int printArea();
+	private:
+		int length;
+		int width;
+	
+};
+
+void Rectangle::setLength(int l){
+}
+void Rectangle::setWidth(int w){
+}
+int Rec
+
+int main()
+{
+	
+	    system("PAUSE");   
+    return 0;   
+}
